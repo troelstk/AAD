dfe16149b9dbdc87f4641ccf77265dda5bd3d302
Troels Tang Karlsen <troelstk@gmail.com>
2019-09-02 15:51:17 +0200

    Initial Commit

---
+++ AAD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AAD
+//
+//  Created by Troels Tang Karlsen on 02/09/2019.
+//  Copyright © 2019 Troels Tang Karlsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
